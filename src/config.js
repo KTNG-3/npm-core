ed368347af63a309aac702e1efc867540b0a1e8a
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-03-28 13:42:01 +0700

    1.4.0

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
     const fs = require('fs');
     const ing3kth_init = require('./command/data/init');
 
-    ing3kth_init.execute(false);
+    await ing3kth_init.execute(false);
 
     module.exports = require(process.cwd() + '/ing3kth-config.json');
 })();

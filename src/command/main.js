9a39017dd24ae91340ccfffc4e1604167ee0fd9a
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-03-28 14:13:56 +0700

    1.4.1

--- src/command/main.js
+++ src/command/main.js
@@ -5,22 +5,27 @@ const fs = require('fs');
 const commander = require('commander');
 
 //script
-const commandsFolders = fs.readdirSync('./src/command/data');
-
-for (const file of commandsFolders) {
-    const command = require(`./data/${file}`);
-
-    const newCommand = commander.program
-        .command(command.data.name)
-        .description(command.data.description)
-        .action(command.execute);
-
-    if (command.data.option.length > 0) {
-        for (const option of command.data.option) {
-            newCommand.option(option.name, option.description);
+var commandsFolders;
+try {
+    commandsFolders = fs.readdirSync('./src/command/data');
+}catch(err){
+    commandsFolders = fs.readdirSync('./node_modules/@ing3kth/core/src/command/data');
+}finally {
+    for (const file of commandsFolders) {
+        const command = require(`./data/${file}`);
+    
+        const newCommand = commander.program
+            .command(command.data.name)
+            .description(command.data.description)
+            .action(command.execute);
+    
+        if (command.data.option.length > 0) {
+            for (const option of command.data.option) {
+                newCommand.option(option.name, option.description);
+            }
         }
     }
+    
+    //export
+    commander.program.parse();
 }
-
-//export
-commander.program.parse();

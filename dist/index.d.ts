cebfc1a4532caeba33658e1ba1250f1711c854c7
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-04-25 13:34:39 +0700

    1.8.1

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * as Core from "./export/core";
-export * as Interface from "./export/interface";
-export * as Utils from "./export/utils";
-export { _config as Config } from "./config";
-export { Update } from "./update";
-//# sourceMappingURL=index.d.ts.map
--- dist/utils/Wait.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @param {Number} ms
- * @returns {Promise<Number>}
- */
-declare function wait(ms: number): Promise<number>;
-export { wait };
-//# sourceMappingURL=Wait.d.ts.map
